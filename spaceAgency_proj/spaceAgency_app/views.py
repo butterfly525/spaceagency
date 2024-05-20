143558bb7a9ab5d629ad225f1fda1d5224815cc1
Ananastasia <nastya.khromykh@mail.ru>
2024-05-20 22:39:04 +0800

    Тестовое задание готово!

--- spaceAgency_proj/spaceAgency_app/views.py
+++ spaceAgency_proj/spaceAgency_app/views.py
@@ -4,7 +4,6 @@ from.models import Slider
 def index(request):
 
     sliders = Slider.objects.all()
-    print(sliders)
     return render(request, 'spaceAgency_app/index.html', {'sliders': sliders})
     
 
